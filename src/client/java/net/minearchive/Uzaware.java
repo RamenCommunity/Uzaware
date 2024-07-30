b97bb51bd35a703b4e8dbaa7a9c3f66830c07e2f
windows <102716228+itekomasuzo@users.noreply.github.com>
2024-07-31 00:58:55 +0900

    a

--- src/client/java/net/minearchive/Uzaware.java
+++ src/client/java/net/minearchive/Uzaware.java
@@ -26,5 +26,6 @@ public class Uzaware implements ClientModInitializer {
 	public void onInitializeClient() {
 		LOGGER.info("Uzaware Initialized!");
 		LOGGER.info(serif[new Random().nextInt(4)]);
+		LOGGER.info("Uzaware Initialized!");
 	}
 }
