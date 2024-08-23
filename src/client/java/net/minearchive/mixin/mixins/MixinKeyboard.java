a7c52bcdb5fb386ec8278b4f526ad705961a34d9
kazubon12 <103094882+kazubon12@users.noreply.github.com>
2024-08-23 14:09:39 +0900

    無変換キー反応しないでください

--- src/client/java/net/minearchive/mixin/mixins/MixinKeyboard.java
+++ src/client/java/net/minearchive/mixin/mixins/MixinKeyboard.java
@@ -3,6 +3,7 @@ package net.minearchive.mixin.mixins;
 import net.minearchive.Uzaware;
 import net.minearchive.module.Module;
 import net.minecraft.client.Keyboard;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,6 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinKeyboard {
     @Inject(method = "onKey", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;onKeyPressed(Lnet/minecraft/client/util/InputUtil$Key;)V"))
     public void onKeyPressed(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+        if (key == GLFW.GLFW_KEY_UNKNOWN) return;
         Uzaware.moduleManager.modules.stream().filter(m -> m.keybind == key).forEach(Module::toggle);
     }
 }
