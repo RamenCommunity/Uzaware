26cc70994a638508489a106ceaf608bb3a6bcbc3
Hagemelw <126488525+Hagemelw@users.noreply.github.com>
2024-09-15 01:18:51 +0900

    計算したい！

---
+++ src/client/java/net/minearchive/module/modules/misc/SuperIQmath.java
@@ -0,0 +1,9 @@
+package net.minearchive.module.modules.misc;
+
+import net.minearchive.module.Category;
+import net.minearchive.module.Module;
+import net.minearchive.module.ModuleInfo;
+
+@ModuleInfo(name = "SuperIQMath", category = Category.MISC)
+public class SuperIQmath extends Module {
+}
