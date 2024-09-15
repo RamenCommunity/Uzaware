251dfe3bbf7cb1d64562e75407b6ffe02a0fc3e2
Kuromanzyu <ffnrr501@outlook.jp>
2024-09-15 22:47:35 +0900

    影追加 色変え

--- src/client/java/net/minearchive/module/modules/hud/WaterMarkModule.java
+++ src/client/java/net/minearchive/module/modules/hud/WaterMarkModule.java
@@ -6,6 +6,7 @@ import net.minearchive.event.events.Render2DStartEvent;
 import net.minearchive.module.Category;
 import net.minearchive.module.HudModule;
 import net.minearchive.module.ModuleInfo;
+import net.minearchive.setting.settings.EnumSetting;
 import net.minearchive.util.NanoVGUtils;
 import net.minearchive.util.SimpleColor;
 import net.minearchive.util.easing.AnimateValue;
@@ -17,23 +18,36 @@ import java.util.Calendar;
 @ModuleInfo(name = "WaterMark", category = Category.HUD)
 public class WaterMarkModule extends HudModule {
 
+    public EnumSetting<Uzawa> enumSetting = add(new EnumSetting<>("GUI" , Uzawa.Uzawa1));
+    enum Uzawa {Uzawa1,Uzawa2}
+
     AnimateValue animateValue = new AnimateValue(EnumEasing.SINE, 14);
     FPSCalculator fpsCalculator = new FPSCalculator();
 
     @Override
     public void onRender(Render2DStartEvent event) {;
-        Uzaware.nanoVGManager.begin(false);
-        fpsCalculator.render(event);
-        Uzaware.textureManager.createTexture(getClass().getResourceAsStream("/assets/uzaware/texture/reisa.png"), "uzawa");
-        NanoVGUtils.rounded(x, y, 340, 120, 10, SimpleColor.of(0x50ffffff), NanoVGUtils.Pattern.FILL);
-        NanoVGUtils.rounded(x, y, 120, 120, 10, Uzaware.textureManager.getTextures("uzawa", 120, 120), NanoVGUtils.Pattern.FILL);
-        NanoVGUtils.ntr.draw(Uzaware.modName + " v" + Uzaware.version, x + 130, y + 10, 32, 0xff000000);
-        String timeStamp = new SimpleDateFormat("MM/dd HH:mm:ss").format(Calendar.getInstance().getTime());
+        switch (enumSetting.getValue()){
+            case Uzawa1 ->{
+                Uzaware.nanoVGManager.begin(false);
+                fpsCalculator.render(event);
+                NanoVGUtils.shadow(x, y, 340, 120, 10, SimpleColor.of(0x40000000));
+                Uzaware.textureManager.createTexture(getClass().getResourceAsStream("/assets/uzaware/texture/reisa.png"), "uzawa");
+                NanoVGUtils.rounded(x, y, 340, 120, 10, SimpleColor.of(0x50ffffff), NanoVGUtils.Pattern.FILL);
+                NanoVGUtils.rounded(x, y, 120, 120, 10, Uzaware.textureManager.getTextures("uzawa", 120, 120), NanoVGUtils.Pattern.FILL);
+                NanoVGUtils.ntr.draw(Uzaware.modName + " v" + Uzaware.version, x + 130, y + 10, 32, 0xffffffff);
+                String timeStamp = new SimpleDateFormat("MM/dd HH:mm:ss").format(Calendar.getInstance().getTime());
 
-        animateValue.draw(x + 130, y + 40, 32, timeStamp, 300, SimpleColor.of(0xff000000));
+                animateValue.draw(x + 130, y + 40, 32, timeStamp, 300, SimpleColor.of(0xffffffff));
 
-        NanoVGUtils.ntr.draw("FPS: " + fpsCalculator.currentFPS, x + 130, y + 70, 32, 0xff000000);
-        Uzaware.nanoVGManager.end();
+                NanoVGUtils.ntr.draw("FPS: " + fpsCalculator.currentFPS, x + 130, y + 70, 32, 0xffffffff);
+                Uzaware.nanoVGManager.end();
+            }
+            case Uzawa2 ->{
+                Uzaware.nanoVGManager.begin(false);
+                NanoVGUtils.ntr.draw("UzaWere v"  + Uzaware.version, x + 5, y + 5, 32, 0xffffffff);
+                Uzaware.nanoVGManager.end();
+            }
+        }
     }
 
     @Override
