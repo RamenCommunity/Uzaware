4daa3122a7c7c145e28456b01533f9516a3b1e8b
MineArchive <102400088+minearchive@users.noreply.github.com>
2024-09-15 21:46:17 +0900

    fea: FAST FPS:nerd:

--- src/client/java/net/minearchive/module/modules/hud/WaterMarkModule.java
+++ src/client/java/net/minearchive/module/modules/hud/WaterMarkModule.java
@@ -1,5 +1,6 @@
 package net.minearchive.module.modules.hud;
 
+import com.google.common.eventbus.Subscribe;
 import net.minearchive.Uzaware;
 import net.minearchive.event.events.Render2DStartEvent;
 import net.minearchive.module.Category;
@@ -17,10 +18,12 @@ import java.util.Calendar;
 public class WaterMarkModule extends HudModule {
 
     AnimateValue animateValue = new AnimateValue(EnumEasing.SINE, 14);
+    FPSCalculator fpsCalculator = new FPSCalculator();
 
     @Override
     public void onRender(Render2DStartEvent event) {;
         Uzaware.nanoVGManager.begin(false);
+        fpsCalculator.render(event);
         Uzaware.textureManager.createTexture(getClass().getResourceAsStream("/assets/uzaware/texture/reisa.png"), "uzawa");
         NanoVGUtils.rounded(x, y, 340, 120, 10, SimpleColor.of(0x50ffffff), NanoVGUtils.Pattern.FILL);
         NanoVGUtils.rounded(x, y, 120, 120, 10, Uzaware.textureManager.getTextures("uzawa", 120, 120), NanoVGUtils.Pattern.FILL);
@@ -29,11 +32,68 @@ public class WaterMarkModule extends HudModule {
 
         animateValue.draw(x + 130, y + 40, 32, timeStamp, 300, SimpleColor.of(0xff000000));
 
+        NanoVGUtils.ntr.draw("FPS: " + fpsCalculator.currentFPS, x + 130, y + 70, 32, 0xff000000);
         Uzaware.nanoVGManager.end();
     }
 
     @Override
     public void onDrag(float x, float y, float deltaX, float deltaY) {
         super.onDrag(x, y, deltaX, deltaY);
+    }
+
+
+    public static class FPSCalculator {
+        private int currentFPS;
+        private final FPS[] timers;
+
+        public FPSCalculator() {
+            currentFPS = 0;
+            timers = new FPS[20];
+            long startTime = System.nanoTime();
+            for (int i = 0; i < timers.length; i++) {
+                long plus = startTime + ((long) i * 1000000000 / timers.length);
+                timers[i] = new FPS(plus);
+            }
+        }
+
+        @Subscribe
+        public void render(Render2DStartEvent event) {
+            for (FPS fps : timers) {
+                fps.updateFPSCount();
+                if (fps.isOver()) {
+                    this.currentFPS = fps.getFpsCount();
+                    fps.updateStartTime();
+                }
+            }
+        }
+
+        public static class FPS {
+            private long startTime;
+            private int fpsCount;
+
+            public FPS(long startTime) {
+                this.startTime = startTime;
+                fpsCount = 0;
+            }
+
+            public void updateFPSCount() {
+                fpsCount++;
+            }
+
+            public boolean isOver() {
+                return System.nanoTime() - startTime >= 1000000000;
+            }
+
+            public void updateStartTime() {
+                while (isOver()) {
+                    startTime += 1000000000;
+                }
+                fpsCount = 0;
+            }
+
+            public int getFpsCount() {
+                return fpsCount;
+            }
+        }
     }
 }
