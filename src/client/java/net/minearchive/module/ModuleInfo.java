03fb2e0bf057255e866ccbde72aa4af765481731
kazubon12 <103094882+kazubon12@users.noreply.github.com>
2024-09-16 20:50:27 +0900

    なんとなく

--- src/client/java/net/minearchive/module/ModuleInfo.java
+++ src/client/java/net/minearchive/module/ModuleInfo.java
@@ -2,10 +2,13 @@ package net.minearchive.module;
 
 import org.lwjgl.glfw.GLFW;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface ModuleInfo {
     String name();
     String description()    default "";
