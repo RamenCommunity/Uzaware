a4dab009b4e874e364bf5c886cdcb18d3502dcc8
MineArchive <102400088+minearchive@users.noreply.github.com>
2024-09-23 12:44:36 +0900

    remove: nowayiforgotremoveunusedimport[2]

--- src/client/java/net/minearchive/module/modules/movement/MovementTweaksModule.java
+++ src/client/java/net/minearchive/module/modules/movement/MovementTweaksModule.java
@@ -4,13 +4,10 @@ import net.minearchive.module.Category;
 import net.minearchive.module.Module;
 import net.minearchive.module.ModuleInfo;
 import net.minearchive.setting.settings.BooleanSetting;
-import net.minecraft.entity.EntityPose;
 
 @ModuleInfo(name = "MovementTweaks", category = Category.MOVEMENT)
 public class MovementTweaksModule extends Module {
     public static MovementTweaksModule INSTANCE;
-
-    public EntityPose pose;
 
     public BooleanSetting noSwim = add(new BooleanSetting("No Block Swim", false));
     public BooleanSetting noRiptide = add(new BooleanSetting("No Riptide", false));
