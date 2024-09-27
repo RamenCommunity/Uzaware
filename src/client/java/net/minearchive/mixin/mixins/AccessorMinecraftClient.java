e515c896302ade405cb890be22185aa3eeacfb9c
MineArchive <102400088+minearchive@users.noreply.github.com>
2024-09-27 14:20:43 +0900

    Spotify関係

---
+++ src/client/java/net/minearchive/mixin/mixins/AccessorMinecraftClient.java
@@ -0,0 +1,11 @@
+package net.minearchive.mixin.mixins;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(MinecraftClient.class)
+public interface AccessorMinecraftClient {
+    @Accessor("thread")
+    Thread getThread();
+}
--- src/client/java/net/minearchive/mixin/mixins/MixinInGameHud.java
+++ src/client/java/net/minearchive/mixin/mixins/MixinInGameHud.java
@@ -1,5 +1,6 @@
 package net.minearchive.mixin.mixins;
 
+import net.minearchive.Uzaware;
 import net.minearchive.event.events.Render2DEndEvent;
 import net.minearchive.event.events.Render2DStartEvent;
 import net.minearchive.manager.ModuleManager;
@@ -19,7 +20,9 @@ import static net.minearchive.Uzaware.EVENT_BUS;
 public class MixinInGameHud {
     @Inject(method = "render", at = @At("HEAD"))
     public void onRenderStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        Uzaware.nanoVGManager.begin(false);
         EVENT_BUS.post(new Render2DStartEvent(context, tickDelta));
+        Uzaware.nanoVGManager.end();
         ModuleManager.INSTANCE.modules.stream().filter(m -> m instanceof HudModule).map(m -> (HudModule) m).forEach(m -> {
             m.x = Math.clamp(0, MinecraftClient.getInstance().getWindow().getWidth() - m.width, m.x);
             m.y = Math.clamp(0, MinecraftClient.getInstance().getWindow().getHeight() - m.height, m.y);
@@ -28,6 +31,8 @@ public class MixinInGameHud {
 
     @Inject(method = "render", at = @At("TAIL"))
     public void onRenderEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        Uzaware.nanoVGManager.begin(false);
         EVENT_BUS.post(new Render2DEndEvent(context, tickDelta));
+        Uzaware.nanoVGManager.end();
     }
 }
