ab9e328c2de04aa90188411fdf08b8f88921b6d4
kazubon12 <103094882+kazubon12@users.noreply.github.com>
2024-10-03 18:30:20 +0900

    add NoFall to MovementTweaksModule

---
+++ src/client/java/net/minearchive/mixin/mixins/IPlayerMoveC2SPacket.java
@@ -0,0 +1,13 @@
+package net.minearchive.mixin.mixins;
+
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(value = PlayerMoveC2SPacket.class)
+public interface IPlayerMoveC2SPacket {
+    @Mutable
+    @Accessor(value = "onGround")
+    void setOnGround(boolean onGround);
+}
--- src/client/java/net/minearchive/module/modules/movement/MovementTweaksModule.java
+++ src/client/java/net/minearchive/module/modules/movement/MovementTweaksModule.java
@@ -1,9 +1,13 @@
 package net.minearchive.module.modules.movement;
 
+import com.google.common.eventbus.Subscribe;
+import net.minearchive.event.events.PacketSendEvent;
+import net.minearchive.mixin.mixins.IPlayerMoveC2SPacket;
 import net.minearchive.module.Category;
 import net.minearchive.module.Module;
 import net.minearchive.module.ModuleInfo;
 import net.minearchive.setting.settings.BooleanSetting;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 @ModuleInfo(name = "MovementTweaks", category = Category.MOVEMENT)
 public class MovementTweaksModule extends Module {
@@ -11,9 +15,17 @@ public class MovementTweaksModule extends Module {
 
     public final BooleanSetting noSwim = add(new BooleanSetting("No Block Swim", false));
     public final BooleanSetting noJumpDelay = add(new BooleanSetting("No Jump Delay", false));
+    public final BooleanSetting noFall = add(new BooleanSetting("No Fall Damage", false));
     public final BooleanSetting noRiptide = add(new BooleanSetting("No Riptide", false));
 
     public MovementTweaksModule() {
         INSTANCE = this;
+    }
+
+    @Subscribe
+    public void onPacketSend(PacketSendEvent event) {
+        if (noFall.getValue() && event.getPacket() instanceof PlayerMoveC2SPacket packet) {
+            ((IPlayerMoveC2SPacket) packet).setOnGround(false);
+        }
     }
 }
