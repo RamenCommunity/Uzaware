13bdc4008de0bdb848d2d053a961a47895f390c8
kazubon12 <103094882+kazubon12@users.noreply.github.com>
2024-10-11 02:06:07 +0900

    a

--- src/client/java/net/minearchive/util/EntityUtils.java
+++ src/client/java/net/minearchive/util/EntityUtils.java
@@ -1,6 +1,7 @@
 package net.minearchive.util;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
 public class EntityUtils {
@@ -10,5 +11,9 @@ public class EntityUtils {
 
     public static Vec3d lastRenderPos(Entity e) {
         return new Vec3d(e.lastRenderX, e.lastRenderY, e.lastRenderZ);
+    }
+
+    public static BlockPos flooredPos(Entity e) {
+        return new BlockPos((int) Math.floor(e.getX()), (int) Math.floor(e.getY()), (int) Math.floor(e.getZ()));
     }
 }
